bcda5844b56af6ad49403cd384b729ba6befb1ce
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 12:45:48 -0800

    Add files via upload

---
+++ Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['kamran Tissori', 'Zia Khan', 'Daniyal Nagori'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\nIt is my pleasure to invite you on my birthday party.\n\nThank You ! ');
+}
---
+++ Guest_List.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_List :string[] = ['kamran Tissori','Zia Khan','Daniyal Nagori'];
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\nIt is my pleasure to invite you on my birthday party.\n\nThank You ! ')
+    
+}
